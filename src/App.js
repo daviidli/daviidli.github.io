4d149032fc11105fb6b2ce6deed1c47bb7c1085b
David Li <b7p0b@ugrad.cs.ubc.ca>
2019-01-14 20:53:13 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Paper from '@material-ui/core/Paper';
+import {greeting} from "./content/data";
+
+const muiTheme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#3490FF"
+        },
+        secondary: {
+            main: "#FF005E",
+        },
+    },
+});
+
+const styles = theme => ({
+    container: {
+        width: window.innerWidth,
+        height: window.innerHeight
+    },
+    paper: {
+        margin: "auto",
+        marginTop: theme.spacing.unit * 3,
+        width: "90%",
+        height: "90%",
+        padding: theme.spacing.unit,
+    }
+});
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            useEmoji: true
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <MuiThemeProvider theme={muiTheme}>
+                    <div className={this.props.classes.container}>
+                        <Paper className={this.props.classes.paper} elevation={1} square={true}>
+                            <div style={{fontSize: 20}} dangerouslySetInnerHTML={{__html: greeting}}>
+
+                            </div>
+                        </Paper>
+                    </div>
+                    <div >
+                        {/*<p>hello</p>*/}
+                    </div>
+                </MuiThemeProvider>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/content/data.js
@@ -0,0 +1,1 @@
+export const greeting = "Hey, my name is David Li. I'm a student in Vancouver 🍁 <br> studying Business and Computer Science at 🏫 UBC.";
