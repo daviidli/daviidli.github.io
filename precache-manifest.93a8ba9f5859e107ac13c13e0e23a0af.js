c4e0a3c12262c0d51c3721e5e0fc848e81f25abd
David Li <d.li.2247@gmail.com>
2020-07-18 13:00:24 -0700

    Updates

---
+++ precache-manifest.93a8ba9f5859e107ac13c13e0e23a0af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34e85eb76a0ffd3ac8d9c46346dcc65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8c5342d16c307b9589a",
+    "url": "/static/css/2.5af4be20.chunk.css"
+  },
+  {
+    "revision": "b29b87f6d602c892bf90",
+    "url": "/static/css/main.b7ba65b3.chunk.css"
+  },
+  {
+    "revision": "f8c5342d16c307b9589a",
+    "url": "/static/js/2.75a1470e.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.75a1470e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b29b87f6d602c892bf90",
+    "url": "/static/js/main.4c34404d.chunk.js"
+  },
+  {
+    "revision": "11575855c0c727951ca8",
+    "url": "/static/js/runtime-main.0d45763b.js"
+  }
+]);
